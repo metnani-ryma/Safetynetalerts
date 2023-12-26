eef1e174edb37d4f9336ae032b1f34e370964c56
metnani-ryma <m.ryma@protonmail.com>
2023-12-27 00:41:15 +0100

    initialisation

---
+++ src/main/java/com/safetynet/alert/SafetynetalertApplication.java
@@ -0,0 +1,13 @@
+package com.safetynet.alert;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SafetynetalertApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SafetynetalertApplication.class, args);
+	}
+
+}
